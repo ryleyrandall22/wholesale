a1ff574ed5615a9dfa7c5f1c3f327b251ac2cb8b
ryleyrandall22 <33163774+ryleyrandall22@users.noreply.github.com>
2019-09-13 13:27:34 -0600

    Updated Firebase Congif
    This should work

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
+  apiKey: "AIzaSyC9AyUAW7gAqF_qv1XQU0KVtrtcsz40IL4",
+  authDomain: "wholesalesite-ryley.firebaseapp.com",
+  databaseURL: "https://wholesalesite-ryley.firebaseio.com",
+  projectId: "wholesalesite-ryley",
   storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  messagingSenderId: "377071757346",
+  appId: "1:377071757346:web:26c841d6edb72560f2827b"
 };
 
 firebase.initializeApp(config);
